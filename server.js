193e7bc58f3c1f58e5f96656628fe66734fc1d3f
Mizanur <jameslev2001@gmail.com>
2022-02-24 07:59:35 +0530

    initial

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
+const PORT = 3300 || process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+//set Template engine
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
+});
