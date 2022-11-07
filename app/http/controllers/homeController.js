2f137ee1d266a6fe1f5f38f9c7a14531e619efb0
Mizanur <jameslev2001@gmail.com>
2022-11-07 16:25:24 +0530

    apply algo to sort the availability

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -2,9 +2,16 @@ const menu = require('../../models/menu');
 function homeController() {
     return {
         async index(req, res) {
+            const available = [], unAvailable = [];
             const food = await menu.find();
+            food.forEach(element => {
+                element.availability == "available" ? available.push(element) : unAvailable.push(element);
+            });
+
+            const foodSorted = [...available, ...unAvailable]
+
             res.header('Cache-Control', 'no-store')
-            res.render("home", { foodItems: food });
+            res.render("home", { foodItems: foodSorted });
         }
     }
 }
