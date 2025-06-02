51273a352c17b8c3090d2bf934ae31d684f751b2
Mizanur <jameslev2001@gmail.com>
2025-06-03 02:40:37 +0530

    fix: +91 phone number issue in order model

--- app/models/orders.js
+++ app/models/orders.js
@@ -8,7 +8,7 @@ const orderSchema = new Schema({
         required: true
     },
     items: { type: Object, required: true },
-    phone: { type: Number, required: true },
+    phone: { type: String, required: true },
     address: { type: String, required: true },
     paymentType: { type: String, default: 'COD' },
     status: { type: String, default: 'order_placed' }
